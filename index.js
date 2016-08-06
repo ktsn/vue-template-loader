0f0105159bea50aa470973aea675f6a9ad6fcd38
ktsn <ktsn55@gmail.com>
2016-08-07 02:04:31 +0900

    Better error handling

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const compiler = require('vue-template-compiler')
 module.exports = function (content) {
   const compiled = compiler.compile(content)
 
-  if (compiled.errors.length > 0) throw compiled.errors
+  compiled.errors.forEach(error => {
+    this.emitError('template syntax error ' + error)
+  })
 
   const output =
     'module.exports = function (options) {\n' +
