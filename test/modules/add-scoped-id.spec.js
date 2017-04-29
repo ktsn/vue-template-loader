3e5eb7e6fe087be1a1f6941b18055a27c9b5e609
ktsn <ktsn55@gmail.com>
2017-04-29 16:29:26 +0900

    Support css source map

--- test/modules/add-scoped-id.spec.js
+++ test/modules/add-scoped-id.spec.js
@@ -1,3 +1,6 @@
+const SourceMapConsumer = require('source-map').SourceMapConsumer
+const Base64 = require('js-base64').Base64
+const sass = require('node-sass')
 const addScopedId = require('../../lib/modules/add-scoped-id')
 
 describe('add scoped id module', () => {
@@ -24,13 +27,77 @@ describe('add scoped id module', () => {
       '@media screen { p[data-v-1] {} }'
     ).then(done)
   })
+
+  it('should generate source map', done => {
+    test(
+      'data-v-1',
+      'div p {}',
+      'div p[data-v-1] {}'
+    ).then(result => {
+      expect(result.map).not.toBeFalsy()
+
+      const smc = new SourceMapConsumer(JSON.parse(result.map))
+      let pos = smc.originalPositionFor({ line: 1, column: 17 })
+      expect(pos.line).toBe(1)
+      expect(pos.column).toBe(0)
+
+      pos = smc.originalPositionFor({ line: 1, column: 18 })
+      expect(pos.line).toBe(1)
+      expect(pos.column).toBe(8)
+
+      done()
+    })
+  })
+
+  it('should consume previous source map', done => {
+    const result = sass.renderSync({
+      file: 'test.css',
+      data: [
+        'div {',
+        '  p {',
+        '    color: red;',
+        '  }',
+        '}'
+      ].join('\n'),
+
+      outputStyle: 'expanded',
+      sourceMapEmbed: true
+    })
+
+    const sourceMapRE = /^([\s\S]+)\n\n\/\*# sourceMappingURL=data:application\/json;base64,(.+) \*\/$/
+    const m = result.css.toString().match(sourceMapRE)
+
+    const css = m[1]
+    const map = JSON.parse(Base64.decode(m[2]))
+
+    test(
+      'data-v-1',
+      css,
+      [
+        'div p[data-v-1] {',
+        '  color: red;',
+        '}'
+      ].join('\n'),
+      map
+    ).then(result => {
+      expect(result.map).not.toBeFalsy()
+
+      const smc = new SourceMapConsumer(JSON.parse(result.map))
+      const pos = smc.originalPositionFor({ line: 2, column: 2 })
+      expect(pos.line).toBe(3)
+      expect(pos.column).toBe(4)
+
+      done()
+    })
+  })
 })
 
-function test (id, input, expected) {
-  return addScopedId(id, input)
+function test (id, input, expected, map) {
+  return addScopedId(id, 'test.css', input, map)
     .then(
       result => {
         expect(result.css).toBe(expected)
+        return result
       },
       err => {
         console.error(err)
