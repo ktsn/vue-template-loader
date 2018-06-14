3626764015607e85c06dc23284d82badf22df913
ktsn <ktsn55@gmail.com>
2018-06-14 13:28:40 +0900

    feat: use @vue/component-compiler-utils to compile template
    BREAKING CHANGE: renamed transformToRequire with transformAssetUrls

--- test/modules/template-compiler.spec.js
+++ test/modules/template-compiler.spec.js
@@ -4,7 +4,7 @@ const compile = require('../../lib/modules/template-compiler')
 describe('template-compiler', () => {
   it('transforms specified el\'s attr to require', () => {
     const actual = compile('<img src="./foo.png">', {
-      transformToRequire: {
+      transformAssetUrls: {
         img: 'src'
       }
     })
@@ -13,12 +13,12 @@ describe('template-compiler', () => {
 
   it('does not transform unspecified attributes', () => {
     const actual = compile('<img src="./foo.png">')
-    expect(actual.code).toMatch(/"src":"\.\/foo\.png"/)
+    expect(actual.code).toMatch(/src: "\.\/foo\.png"/)
   })
 
   it('transforms "~" to module name require', () => {
     const actual = compile('<img src="~foo.png">', {
-      transformToRequire: {
+      transformAssetUrls: {
         img: 'src'
       }
     })
